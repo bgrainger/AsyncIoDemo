3b15cc264c1685af50515c907b7f0b7275175ec1
Bradley Grainger <bgrainger@gmail.com>
2010-02-06 23:45:12 -0800

    Async implementation uses all three shipping services.

--- net35/src/Service/ShippingRatesProvider.cs
+++ net35/src/Service/ShippingRatesProvider.cs
@@ -8,6 +8,8 @@ namespace Service
 {
 	public partial class ShippingRatesProvider : IShippingRatesProvider
 	{
+		#region Synchronous Implementation
+
 		public ShippingRate[] GetShippingRatesSync(decimal weight, string originZipCode, string destinationZipCode)
 		{
 			List<ShippingRate> rates = new List<ShippingRate>();
@@ -35,13 +37,27 @@ namespace Service
 				return GetUspsRates(response);
 		}
 
+		#endregion
+
+		#region Asynchronous Implementation
+
 		public IAsyncResult BeginGetShippingRatesAsync(decimal weight, string originZipCode, string destinationZipCode, AsyncCallback callback, object state)
 		{
+			// create object that will store results
 			AsyncResult<ShippingRate[]> asyncResult = new AsyncResult<ShippingRate[]>(callback, state);
-			
-			WebRequest request = CreateUspsRequest(weight, originZipCode, destinationZipCode);
-			request.BeginGetResponse(ar => UspsCallback(ar, request, asyncResult), null);
+			ShippingRatesRequest request = new ShippingRatesRequest(asyncResult, 3);
 
+			// launch requests in parallel
+			WebRequest fedExRequest = CreateFedExRequest(weight, originZipCode, destinationZipCode);
+			fedExRequest.BeginGetResponse(ar => FedExCallback(request, fedExRequest, ar), null);
+
+			WebRequest upsRequest = CreateUpsRequest(weight, originZipCode, destinationZipCode);
+			upsRequest.BeginGetResponse(ar => UpsCallback(request, upsRequest, ar), null);
+
+			WebRequest uspsRequest = CreateUspsRequest(weight, originZipCode, destinationZipCode);
+			uspsRequest.BeginGetResponse(ar => UspsCallback(request, uspsRequest, ar), null);
+
+			// return IAsyncResult implementation to client
 			return asyncResult;
 		}
 
@@ -50,10 +66,58 @@ namespace Service
 			return ((AsyncResult<ShippingRate[]>) asyncResult).EndInvoke();
 		}
 
-		private static void UspsCallback(IAsyncResult asyncResult, WebRequest request, AsyncResult<ShippingRate[]> ratesAsyncResult)
+		private static void FedExCallback(ShippingRatesRequest request, WebRequest webRequest, IAsyncResult asyncResult)
 		{
-			using (WebResponse response = request.EndGetResponse(asyncResult))
-				ratesAsyncResult.Finish(GetUspsRates(response), false);
+			using (WebResponse response = webRequest.EndGetResponse(asyncResult))
+				request.AddRates(GetFedExRates(response));
 		}
+
+		private static void UpsCallback(ShippingRatesRequest request, WebRequest webRequest, IAsyncResult asyncResult)
+		{
+			using (WebResponse response = webRequest.EndGetResponse(asyncResult))
+				request.AddRates(GetUpsRates(response));
+		}
+
+		private static void UspsCallback(ShippingRatesRequest request, WebRequest webRequest, IAsyncResult asyncResult)
+		{
+			using (WebResponse response = webRequest.EndGetResponse(asyncResult))
+				request.AddRates(GetUspsRates(response));
+		}
+
+		/// <summary>
+		/// <see cref="ShippingRatesRequest"/> represents the state of one request to GetShippingRatesAsync.
+		/// </summary>
+		private class ShippingRatesRequest
+		{
+			public ShippingRatesRequest(AsyncResult<ShippingRate[]> asyncResult, int outstandingRequests)
+			{
+				m_lock = new object();
+				m_rates = new List<ShippingRate>();
+				m_asyncResult = asyncResult;
+				m_outstandingRequests = outstandingRequests;
+			}
+
+			/// <summary>
+			/// Adds shipping rates to this object's list of results.
+			/// </summary>
+			/// <param name="rates">The shipping rates to add.</param>
+			/// <remarks>This method is thread-safe.</remarks>
+			public void AddRates(IEnumerable<ShippingRate> rates)
+			{
+				lock (m_lock)
+				{
+					m_rates.AddRange(rates);
+					if (--m_outstandingRequests == 0)
+						m_asyncResult.Finish(m_rates.ToArray(), false);
+				}
+			}
+
+			readonly object m_lock;
+			readonly List<ShippingRate> m_rates;
+			readonly AsyncResult<ShippingRate[]> m_asyncResult;
+			int m_outstandingRequests;
+		}
+
+		#endregion
 	}
 }
