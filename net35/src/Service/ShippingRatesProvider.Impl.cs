8b423bca5c05c74913e9efb7169956c299335d79
Bradley Grainger <bgrainger@gmail.com>
2010-02-06 23:31:02 -0800

    Added all three shipping providers; moved demo implementation to separate file for pedagogical purposes.

---
+++ net35/src/Service/ShippingRatesProvider.Impl.cs
@@ -0,0 +1,81 @@
+﻿
+using System;
+using System.Net;
+using System.Threading;
+
+namespace Service
+{
+	partial class ShippingRatesProvider
+	{
+		private static WebRequest CreateFedExRequest(decimal weight, string originZipCode, string destinationZipCode)
+		{
+			// In a real application, this would create the request that would retrieve rates from FedEx.
+			Uri uri = new Uri("http://www.fedex.com/");
+			return WebRequest.Create(uri);
+		}
+
+		private static ShippingRate[] GetFedExRates(WebResponse response)
+		{
+			// In a real application, this method would parse the response from FedEx and return actual shipping rates.
+			// Because this is a demo, we simply make up some rates (and sleep to simulate network delay).
+
+			Thread.Sleep(c_demoDelay);
+
+			Random random = new Random();
+			return new[]
+			{
+				new ShippingRate { Name = "FedEx Standard Overnight", Cost = random.Next(1500, 5000) / 100m },
+				new ShippingRate { Name = "FedEx 2Day", Cost = random.Next(1000, 2000) / 100m },
+			};
+		}
+
+		private static WebRequest CreateUpsRequest(decimal weight, string originZipCode, string destinationZipCode)
+		{
+			// In a real application, this would create the request that would retrieve rates from UPS.
+			Uri uri = new Uri("http://www.ups.com/");
+			return WebRequest.Create(uri);
+		}
+
+		private static ShippingRate[] GetUpsRates(WebResponse response)
+		{
+			// In a real application, this method would parse the response from UPS and return actual shipping rates.
+			// Because this is a demo, we simply make up some rates (and sleep to simulate network delay).
+
+			Thread.Sleep(c_demoDelay);
+
+			Random random = new Random();
+			return new[]
+			{
+				new ShippingRate { Name = "UPS Next Day Air", Cost = random.Next(2500, 5000) / 100m },
+				new ShippingRate { Name = "UPS 2nd Day Air", Cost = random.Next(1500, 4000) / 100m },
+				new ShippingRate { Name = "UPS 3 Day Select", Cost = random.Next(500, 2000) / 100m },
+				new ShippingRate { Name = "UPS Ground", Cost = random.Next(300, 1000) / 100m },
+			};
+		}
+
+		private static WebRequest CreateUspsRequest(decimal weight, string originZipCode, string destinationZipCode)
+		{
+			// In a real application, this would create the request that would retrieve rates from USPS.
+			Uri uri = new Uri("http://www.usps.com/");
+			return WebRequest.Create(uri);
+		}
+
+		private static ShippingRate[] GetUspsRates(WebResponse response)
+		{
+			// In a real application, this method would parse the response from USPS and return actual shipping rates.
+			// Because this is a demo, we simply make up some rates (and sleep to simulate network delay).
+
+			Thread.Sleep(c_demoDelay);
+
+			Random random = new Random();
+			return new[]
+			{
+				new ShippingRate { Name = "Express Mail", Cost = random.Next(1000, 2000) / 100m },
+				new ShippingRate { Name = "Priority Mail", Cost = random.Next(500, 1500) / 100m },
+				new ShippingRate { Name = "First-Class Mail", Cost = random.Next(200, 800) / 100m },
+			};
+		}
+
+		const int c_demoDelay = 300;
+	}
+}
--- net35/src/Service/ShippingRatesProvider.cs
+++ net35/src/Service/ShippingRatesProvider.cs
@@ -2,25 +2,44 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Threading;
 using Logos.Utility.Threading;
 
 namespace Service
 {
-	public class ShippingRatesProvider : IShippingRatesProvider
+	public partial class ShippingRatesProvider : IShippingRatesProvider
 	{
 		public ShippingRate[] GetShippingRatesSync(decimal weight, string originZipCode, string destinationZipCode)
 		{
 			List<ShippingRate> rates = new List<ShippingRate>();
-			rates.AddRange(GetUspsShippingRates(weight, originZipCode, destinationZipCode));
+			rates.AddRange(GetFedExRates(weight, originZipCode, destinationZipCode));
+			rates.AddRange(GetUpsRates(weight, originZipCode, destinationZipCode));
+			rates.AddRange(GetUspsRates(weight, originZipCode, destinationZipCode));
 			return rates.ToArray();
+		}
+
+		private static ShippingRate[] GetFedExRates(decimal weight, string originZipCode, string destinationZipCode)
+		{
+			using (WebResponse response = CreateFedExRequest(weight, originZipCode, destinationZipCode).GetResponse())
+				return GetFedExRates(response);
+		}
+
+		private static ShippingRate[] GetUpsRates(decimal weight, string originZipCode, string destinationZipCode)
+		{
+			using (WebResponse response = CreateUpsRequest(weight, originZipCode, destinationZipCode).GetResponse())
+				return GetUpsRates(response);
+		}
+
+		private static ShippingRate[] GetUspsRates(decimal weight, string originZipCode, string destinationZipCode)
+		{
+			using (WebResponse response = CreateUspsRequest(weight, originZipCode, destinationZipCode).GetResponse())
+				return GetUspsRates(response);
 		}
 
 		public IAsyncResult BeginGetShippingRatesAsync(decimal weight, string originZipCode, string destinationZipCode, AsyncCallback callback, object state)
 		{
 			AsyncResult<ShippingRate[]> asyncResult = new AsyncResult<ShippingRate[]>(callback, state);
 			
-			WebRequest request = CreateUspsRequest();
+			WebRequest request = CreateUspsRequest(weight, originZipCode, destinationZipCode);
 			request.BeginGetResponse(ar => UspsCallback(ar, request, asyncResult), null);
 
 			return asyncResult;
@@ -34,38 +53,7 @@ namespace Service
 		private static void UspsCallback(IAsyncResult asyncResult, WebRequest request, AsyncResult<ShippingRate[]> ratesAsyncResult)
 		{
 			using (WebResponse response = request.EndGetResponse(asyncResult))
-				ratesAsyncResult.Finish(CreateUspsRates(), false);
+				ratesAsyncResult.Finish(GetUspsRates(response), false);
 		}
-
-		private static ShippingRate[] GetUspsShippingRates(decimal weight, string originZipCode, string destinationZipCode)
-		{
-			using (WebResponse response = CreateUspsRequest().GetResponse())
-				return CreateUspsRates();
-		}
-
-		private static WebRequest CreateUspsRequest()
-		{
-			// In a real application, this would create the REST request that would retrieve rates from USPS.
-			Uri uri = new Uri("http://www.usps.com/");
-			return WebRequest.Create(uri);
-		}
-
-		private static ShippingRate[] CreateUspsRates()
-		{
-			// In a real application, this method would parse the response from USPS and return actual shipping rates.
-			// Because this is a demo, we simply make up some rates (and sleep to simulate network delay).
-
-			Thread.Sleep(c_demoDelay);
-
-			Random random = new Random();
-			return new[]
-			{
-				new ShippingRate { Name = "Express Mail", Cost = random.Next(1000, 2000) / 100m },
-				new ShippingRate { Name = "Priority Mail", Cost = random.Next(500, 1500) / 100m },
-				new ShippingRate { Name = "First-Class Mail", Cost = random.Next(200, 800) / 100m },
-			};
-		}
-
-		const int c_demoDelay = 300;
 	}
 }
