2cda68ec5d3dbec08b1ec750fe06b322a351876a
Bradley Grainger <bgrainger@gmail.com>
2010-02-06 21:43:17 -0800

    First version of USPS client.

--- net35/src/Service/ShippingRatesProvider.cs
+++ net35/src/Service/ShippingRatesProvider.cs
@@ -1,5 +1,11 @@
 ﻿
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
 using Logos.Utility.Threading;
 
 namespace Service
@@ -8,10 +14,50 @@ namespace Service
 	{
 		public ShippingRate[] GetShippingRatesSync(decimal weight, string originZipCode, string destinationZipCode)
 		{
-			return new[]
+			List<ShippingRate> rates = new List<ShippingRate>();
+			rates.AddRange(GetUspsShippingRates(weight, originZipCode, destinationZipCode));
+			return rates.ToArray();
+		}
+
+		private static ShippingRate[] GetUspsShippingRates(decimal weight, string originZipCode, string destinationZipCode)
+		{
+			// convert to pounds and ounces
+			decimal pounds = Math.Floor(weight);
+			decimal ounces = Math.Round((weight - pounds) * 16m);
+
+			// build XML document for USPS API
+			ApiKeys apiKeys = ApiKeys.Load();
+			XDocument requestDocument = new XDocument(
+				new XElement("RateV3Request", new XAttribute("USERID", apiKeys.UspsUsername),
+					new XElement("Package", new XAttribute("ID", "1"),
+						new XElement("Service", "FIRST CLASS"),
+						new XElement("FirstClassMailType", "PARCEL"),
+						new XElement("ZipOrigination", originZipCode),
+						new XElement("ZipDestination", destinationZipCode),
+						new XElement("Pounds", pounds),
+						new XElement("Ounces", ounces),
+						new XElement("Size", "REGULAR"))));
+			
+			// save XML to string (on one line)
+			StringBuilder sb = new StringBuilder();
+			using (XmlWriter writer = XmlWriter.Create(sb))
+				requestDocument.WriteTo(writer);
+			string requestXml = sb.ToString();
+
+			// make web request; get result
+			Uri uri = new Uri("http://testing.shippingapis.com/ShippingAPITest.dll?API=RateV3&XML=" + requestXml);
+			WebRequest request = WebRequest.Create(uri);
+			using (WebResponse response = request.GetResponse())
+			using (Stream stream = response.GetResponseStream())
+			using (XmlReader reader = XmlReader.Create(stream))
 			{
-				new ShippingRate { Name = "Carrier Pigeon", Cost = 10 },
-			};
+				XDocument responseDocument = XDocument.Load(reader);
+				string xml = responseDocument.ToString();
+				int n = xml.Length;
+			}
+
+			// results not available yet, due to testing server limitations
+			return new ShippingRate[0];
 		}
 
 		public IAsyncResult BeginGetShippingRatesAsync(decimal weight, string originZipCode, string destinationZipCode, AsyncCallback callback, object state)
